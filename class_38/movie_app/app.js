371ac4211aa6ec530e41ee7aa3ad12e8244c4869
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-22 14:37:09 +0530

    node

--- class_38/movie_app/app.js
+++ class_38/movie_app/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  
 
 let input = document.querySelector('input');
@@ -55,3 +56,62 @@ while(list.firstChild){
 
     }
 }
+=======
+
+
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let list = document.querySelector('#list');
+
+
+
+btn.addEventListener('click' , function(){
+    let searchtext = input.value;
+    let data  = fetchData(searchtext);
+    input.value = "";
+
+})
+
+function fetchData(searchtext){
+
+    //fetch
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${searchtext}`)
+    .then(function(res){
+        return res.json();
+
+    })
+    .then(function(data){
+// console.log(data)
+  manipulateDom(data);
+    })
+    //axois
+    // axios.get(`https://api.tvmaze.com/search/shows?q=${searchtext}`)
+    // .then(function(respones){
+    //     console.log(respones.data)
+    //     maipulateDom(respones.data)
+          
+    // }) 
+}
+
+function manipulateDom(allthedata){
+    //to remove the already present movie
+while(list.firstChild){
+    list.firstChild.remove();
+}
+
+
+
+    for(let data of allthedata){
+        let figure = document.createElement('figure');
+        figure.innerHTML = `
+        <img src=${data.show.image.medium} />
+        <h2> Name: ${data.show.name} </h2>
+         <h5> Language: ${data.show.language}</h5>
+         `
+
+         list.appendChild(figure);
+
+    }
+}
+>>>>>>> 6965488 (node-start)
---
+++ class_39/node-js/index.js
@@ -0,0 +1,12 @@
+let pi = 3.145
+
+let Favnumber = 7
+function sqr (num){
+return num*num;
+}
+
+let nas = sqr(6)
+
+console.log(nas)
+console.log(pi)
+console.log(Favnumber)
