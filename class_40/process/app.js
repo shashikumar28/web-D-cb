4986eb4a0e13069b82dc64c0b1f15ebc0b049c9e
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-23 13:27:16 +0530

    node-command

---
+++ class_40/process/app.js
@@ -0,0 +1,13 @@
+//console.log(Process.argv);
+
+//let arr = process.argv.slice(2);
+//console.log(arr);
+// for(let item of arr){
+//     console.log(item)
+// }
+
+let n = process.argv.pop();
+//console.log(n);
+for(item = 1 ; item <= n; item++){
+    console.log(item)
+}
