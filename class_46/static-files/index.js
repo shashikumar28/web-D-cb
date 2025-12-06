f8066f7522e3edf0dea53ca88974ab20b021d811
Shashi Kumar <sammysingh920518@gmail.com>
2025-12-06 17:59:28 +0530

    static-files

---
+++ class_46/static-files/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const path = require ('path');
+
+app.set('view engine' , 'ejs')
+app.set('views' , path.join(__dirname , 'views'));
+
+//static-> har req -> middlewear
+app.use(express.static(path.join(__dirname, 'public')))
+
+//route -> page -> styling (public -> css -> style.css)
+app.get('/whatever' , (req , res) =>{
+    res.render('whatever')
+})
+
+//route. -> page. -> styling(style.css , bootstrap) + DRY PRINCIPAL
+app.get('/horror' , (req ,res)=>{
+    res.render('horror')
+})
+
+let PORT = 8080;
+app.listen(PORT , ()=>{
+    console.log(`server is connected at ${PORT}`)
+})
